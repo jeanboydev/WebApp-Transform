460753635a16a788549fdac17a56502f204e9fbf
jeanboy <jeanboy@foxmail.com>
2019-09-04 09:52:30 +0800

    update

--- Test.java
+++ Test.java
@@ -1,5 +1,52 @@
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadPoolExecutor;
+
 class Test {
     public static void main(String[] args) {
-        Transform.run("/Users/next/Work/Web-App/WeChat/smart_cw", "/Users/next/Work/Web-App/Test");
+        Transform.run("/Users/next/Work/Web-App/WeChat/smart_cw", "/Users/next/Work/Web-App/Alipay/smart_cw");
+
+
+//        LinkedList<String> dataList = new LinkedList<>(); // 创建 LinkedList
+//        dataList.add("test"); // 添加数据
+//        dataList.add(1, "test1"); // 指定位置，添加数据
+//        dataList.addFirst("first"); // 添加数据到头部
+//        dataList.addLast("last"); // 添加数据到尾部
+//        dataList.get(0); // 获取指定位置数据
+//        dataList.getFirst(); // 获取头部数据
+//        dataList.getLast(); // 获取尾部数据
+//        dataList.remove(1); // 移除指定位置的数据
+//        dataList.removeFirst(); // 移除头部数据
+//        dataList.removeLast(); // 移除尾部数据
+//        dataList.clear(); // 清空数据
+
+//        Hashtable<String, String> hashtable = new Hashtable<>();
+//
+//        ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>();
+//        concurrentHashMap.put("aa", "aa");
+//
+//        LinkedHashMap<String, String> linkedHashMap = new LinkedHashMap<>();
+
+//        TreeMap<String, String> treeMap = new TreeMap<>();
+//        treeMap.put("a","a");
+//        treeMap.put("b","b");
+//        treeMap.put("c","c");
+//        treeMap.put("d","d");
+//        treeMap.put("e","e");
+//        System.out.println(treeMap.toString());
+//        String value = treeMap.get("a");
+//        treeMap.remove("a");
+//        treeMap.clear();
+
+//        ThreadPoolExecutor;
+//        StringBuilder stringBuilder=new StringBuilder();
+
+//        Integer integer=Integer.valueOf(1);
+
     }
+
+
 }
